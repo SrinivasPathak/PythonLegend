0394ea9049fd47b1946b9760c5646405c2ae2e15
Srinivas Pathak <pathakshrinivas011@gmail.com>
2025-03-11 01:05:28 +0530

    Added Introduction file to the Repository

---
+++ Introduction_to_Python_Statements.py
@@ -0,0 +1,18 @@
+# Introduction to Python Statements
+
+# In this lecture we will be doing a quick overview of Python Statements. This lecture will emphasize differences between Python and other languages such as C++.
+
+#There are two reasons we take this approach for learning the context of Python Statements:
+
+# 1.) If you are coming from a different language this will rapidly accelerate your understanding of Python.
+# 2.) Learning about statements will allow you to be able to read other languages more easily in the future.
+
+
+a = 3
+b = 1
+
+if a>b:
+    a=2
+    b=4
+    print (a)
+    print (b)
